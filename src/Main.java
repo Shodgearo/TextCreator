9597158f7a82de1a13200cc323405baa5555a1b9
Shodgearo <Shodkvest@gmail.com>
2018-02-27 00:30:23 +0600

    Minimum function. Create text area

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,32 @@
 // This program create and edit text files
-public class Main {
-    public static void main(String[] args) {
 
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame {
+    private final int ROWS = 100;
+    private final int COLS = 400;
+    private JTextArea textArea;
+
+    public static void main(String[] args) {
+        new Main();
+    }
+
+    public Main () {
+        textArea = new JTextArea(COLS, ROWS);
+        JScrollPane scroll = new JScrollPane(textArea);
+
+        add(scroll);
+
+        initFrame();
+    }
+
+    private void initFrame() {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Text Editor");
+        setSize(800, 600);
+//        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
     }
 }
